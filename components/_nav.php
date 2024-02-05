d56379a76d642e820e6f21ec0a418f71aec35ee1
Mayank Rajput <117891351+MynKRjpT@users.noreply.github.com>
2024-02-05 22:43:02 +0530

    Starting login form pptoject in php

---
+++ components/_nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+      <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="components\p-icon.jpg" height="28px" alt="logo"> i-Portal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/login-form/signup.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/login-form/Login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/login-form/welcome.php">Sign Up</a>
+        </li>
+       
+      
+      </ul>
+
+    </div>
+  </div>
+</nav>
