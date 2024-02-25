a13c6cf6ba3713809a7d3c2aee9e2f9ace823e4e
Mayank Rajput <117891351+MynKRjpT@users.noreply.github.com>
2024-02-25 12:05:15 +0530

    Welcome Page Editing

--- welcome.php
+++ welcome.php
@@ -9,28 +9,60 @@ exit;
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome <?php echo $_SESSION['username']?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-  <?php
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+    body {
+        background-image: url('components/wlcm-bg.jpg');
+        /* Using forward slashes */
+        background-size: cover;
+        background-position: center;
+        /* display: flex; /* Set display to flex */
+        /* justify-content: center; Center horizontally */
+        /* align-items: center; */
+        /* height: 100vh;  */
+        /* Optionally, you can add more styles like background color, etc. */
+    }
+
+    .card-bg {
+        background-image: url('components/card-bg.jpg');
+        /* Change path to your card background image */
+        background-size: cover;
+        background-position: center;
+        /* Optionally, you can add more styles like background color, etc. */
+    }
+    </style>
+</head>
+
+<body>
+    <?php
     require 'components/_nav.php'
     ?>
-  <div class="card">
-  <div class="card-header">
-    Welcome-
-  </div>
-  <div class="card-body">
-    <blockquote class="blockquote mb-0">
-      <p><?php echo $_SESSION['username']?></p>
-      <footer class="blockquote-footer">This project is created by- <cite title="Source Title">Mayank Rajput</cite></footer>
-    </blockquote>
-  </div>
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-  </body>
+    <div class="container d-flex justify-content-center ">
+        <div class="card border-success mb-3 card text-center mt-5 card-bg" style="max-width: 25rem;">
+            <div class="card-header bg-transparent border-success"> <strong>Welcome to our Home Page-</strong></div>
+            <div class="card-body text-success">
+                <blockquote class="blockquote mb-0">
+                    <p> <strong><?php echo $_SESSION['username']?></strong> </p>
+                    <footer class="blockquote-footer">This project is created by- <cite title="Source Title">Mayank
+                            Rajput</cite></footer>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
